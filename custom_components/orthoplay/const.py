dd46e8ee794e55a740c6f3c3736d5b5d6a75ded7
Vidar Gundersen <vidarg@fastmail.fm>
2026-05-14 16:46:57 +0200

    Sort source icons by capabilities

--- custom_components/orthoplay/const.py
+++ custom_components/orthoplay/const.py
@@ -7,13 +7,13 @@ DEFAULT_PORT = 8081
 DEFAULT_NAME = "OD-11"
 
 SOURCE_ICONS: dict[str | None, str] = {
-    None:        "mdi:speaker",
-    "AirPlay":   "mdi:cast-audio-variant",
     "Spotify":   "mdi:spotify",
     "Playlist":  "mdi:playlist-music",
-    "Line in":   "mdi:audio-input-stereo-minijack",
-    "Optical":   "mdi:toslink",
+    "AirPlay":   "mdi:cast-audio-variant",
     "Bluetooth": "mdi:bluetooth",
+    "Optical":   "mdi:toslink",
+    "Line in":   "mdi:audio-input-stereo-minijack",
     "Radio":     "mdi:radio",
     "Disk":      "mdi:waveform",
+    None:        "mdi:speaker",
 }
